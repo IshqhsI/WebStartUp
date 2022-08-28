07a9ce55ed991bdd6b68ba4b44c1366a0afd1e62
IshqhsI <oneridh18@github.com>
2022-08-29 00:17:56 +0700

    ubah parameter method construct

--- index.php
+++ index.php
@@ -6,10 +6,13 @@ class Produk {
            $jumlahBarang,
            $hargaBarang;
     
-    public function __construct (Produk $produk) {
+    public function __construct () {
         $this->namaBarang = $namaBarang;
         $this->kodeBarang = $kodeBarang;
         $this->jumlahBarang = $jumlahBarang;
         $this->hargaBarang = $hargaBarang;
     }
 }
+
+
+
