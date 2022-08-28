3b8921450f699af82548c6789765ba77acc3b93d
IshqhsI <oneridh18@github.com>
2022-08-29 00:13:59 +0700

    Menambahkan kelas produk

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <?php 
 
-echo "Hello World";
+class Produk {
+    public $namaBarang, 
+           $kodeBarang,
+           $jumlahBarang,
+           $hargaBarang;
+    
+    public function __construct (Produk $produk) {
+        $this->namaBarang = $namaBarang;
+        $this->kodeBarang = $kodeBarang;
+        $this->jumlahBarang = $jumlahBarang;
+        $this->hargaBarang = $hargaBarang;
+    }
+}
