4fdabe8437736dcde293f67ab1933e72a40d68cc
IshqhsI <oneridh18@github.com>
2022-08-29 16:50:59 +0700

    Menambahkan produk2 untuk mencoba menampilkan hasil dari object dengan kelas

--- index.php
+++ index.php
@@ -21,9 +21,13 @@ class Show {
     }
 }
 
-$produk1 = new produk("Tenda 4P Double Layer", "ten2pdoulay", 1, 45000);
+$produk1 = new produk("Tenda 2P Double Layer", "ten2pdoulay", 1, 45000);
+$produk2 = new produk("Tenda 4P Double Layer", "ten4pdoulay", 1, 40000);
 
 $show = new Show();
 $show = $show->Show($produk1);
 echo $show . "<br>";
-var_dump($show);
+
+$show = new Show();
+$show = $show->Show($produk2);
+echo $show;
