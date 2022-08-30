4743059212a1bf22ca8265db3899d770a3ae099c
IshqhsI <oneridh18@github.com>
2022-08-30 19:01:28 +0700

    Mengisi class yang baru ditambahkan dan mengubah beberapa parameter dan properti serta menambahkan beberapa method

--- index.php
+++ index.php
@@ -6,11 +6,9 @@ class Produk {
 
     protected $hargaBarang;
 
-    public function __construct ($namaBarang, $kodeBarang,  $hargaBarang) {
+    public function __construct ($namaBarang, $kodeBarang) {
         $this->namaBarang = $namaBarang;
         $this->kodeBarang = $kodeBarang;
-        $this->hargaBarang = $hargaBarang;
-
     }
 
     public function getInfoProduk(Produk $produk){
@@ -28,28 +26,49 @@ class Show {
 }
 
 class Tenda extends Produk {
-    public $warna, $jumlahBarang;
+    public $warna, $jumlahBarang = 1, $hargaBarang, $sumHarga;
 
-    public function __construct ($namaBarang, $kodeBarang, $warna, $jumlahBarang, $hargaBarang) {
-
-        parent::__construct($namaBarang, $kodeBarang, $hargaBarang);
-
-        $this->warna = $warna;
+    public function setJumlahBarang($jumlahBarang){
         $this->jumlahBarang = $jumlahBarang;
     }
 
+    public function getJumlahBarang(){
+        return $this->setJumlahBarang($jumlahBarang);
+    }
+
+
+    public function setHargaBarang($hargaBarang){
+        $this->hargaBarang = $hargaBarang;
+    }
+
+    public function getHargaBarang(){
+        return $this->setHargaBarang($hargaBarang);
+    }
+
+    // public function getSumHarga(){ 
+    //     $this->sumHarga = $this->getJumlahBarang() * $this->getHargaBarang();
+    // }
+
+    public function __construct ($namaBarang, $kodeBarang, $warna) {
+        parent::__construct($namaBarang, $kodeBarang);
+        $this->warna = $warna;
+    }
+
     public function getInfoProduk(Produk $produk){
-        $str = " {$this->namaBarang} | 
-                 {$this->kodeBarang} |
-                 {$this->warna} | 
-                 {$this->jumlahBarang} |  
-                 {$this->hargaBarang} ";
+        $str = " Nama Barang : {$this->namaBarang} " . "<br>" . 
+               " Kode Barang : {$this->kodeBarang} " . "<br>" .
+               " Warna : {$this->warna} " . "<br>" . 
+               " Jumlah : {$this->jumlahBarang}" . "<br>" .
+               " Harga : Rp. " . $this->hargaBarang * $this->jumlahBarang;
         return $str;
     }
 }
 
-$produk1 = new Tenda("Tenda 2P Double Layer", "ten2pdoulay", "Merah", 1, 45000);
+$produk1 = new Tenda("Tenda 2P Double Layer", "ten2pdoulay", "Merah");
+echo $produk1->setJumlahBarang(10);
+$produk1->setHargaBarang(40000);
+
+
 
 echo $produk1->getInfoProduk($produk1);
-
 // echo $produk1->harga;
